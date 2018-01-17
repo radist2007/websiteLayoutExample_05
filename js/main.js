031b19f59148cf4866d177478f50ffe1e9c8cd3d
Alex <izofatov2007@gmail.com>
2018-01-17 10:06:47 +0200

    add ScrollToPlugin.js

--- js/main.js
+++ js/main.js
@@ -36,17 +36,15 @@ $(function() {
         .setTween(wipeAnimation)
         // .addIndicators()
         .addTo(controller);
-
-    // controller.scrollTo(function(newpos) {
-    //     TweenMax.to(window, 1, {
-    //         scrollTo: {
-    //         y: newpos
-    //         }
-    //     });
-    // });
+        // console.log(window)
+    controller.scrollTo(function(newpos) {
+        console.log('scrol to: ' + newpos)
+        TweenMax.to(window, 1, {scrollTo: {y: newpos}});
+    });
 
     $("header nav a").on('click', function() {
-        // console.log('click');
+        // window.scrollTo({x: 200});
+        console.log('click');
         var targetSection = $(this).attr('href').substring(1);
         var targetPerc = (targetSection - 1) / ($("header nav a").length - 1);
         var targetPos = scene.scrollOffset() + (scene.duration() * targetPerc);
