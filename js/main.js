dee399475819c6a678a33220e6334315c49a2e34
Alex <izofatov2007@gmail.com>
2018-01-18 05:18:04 +0200

    fix resize

--- js/main.js
+++ js/main.js
@@ -9,22 +9,22 @@ $(function() {
             $('nav').children().removeClass('active');
             $('#firstLink').addClass('active')
           })
-        .fromTo(".blue",  1, {y: "-100%"}, {y: "0%",  ease: Power4.easeInOut})
+        .fromTo(".one",  1, {y: "-100%"}, {y: "0%",  ease: Power4.easeInOut})
         .call(function() {
             $('nav').children().removeClass('active');
             $('#secondLink').addClass('active')
           })
-        .fromTo(".yellow",  1, {x: "-100%"}, {x: "0%",  ease: Power4.easeInOut})
+        .fromTo(".two",  1, {x: "-100%"}, {x: "0%",  ease: Power4.easeInOut})
         .call(function() {
             $('nav').children().removeClass('active');
             $('#thirdLink').addClass('active')
           })
-        .fromTo(".green",   1, {x:  "100%"}, {x: "0%",  ease: Power4.easeInOut})
+        .fromTo(".three",   1, {x:  "100%"}, {x: "0%",  ease: Power4.easeInOut})
         .call(function() {
             $('nav').children().removeClass('active');
             $('#fourthLink').addClass('active')
           }) 
-        .fromTo(".red",     1, {y: "100%"}, {y: "0%",  ease: Power4.easeInOut})
+        .fromTo(".four",     1, {y: "100%"}, {y: "0%",  ease: Power4.easeInOut})
         .call(function() {
             $('nav').children().removeClass('active');
             $('#fivethLink').addClass('active')
